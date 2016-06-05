552cea82fd11ceb275ea7679214f707386824088
Kirill Smirenko <k.smirenko@gmail.com>
2016-06-05 16:43:38 +0300

    Merge upstream

--- src/main/kotlin/patterns/bridge/bridge.kt
+++ src/main/kotlin/patterns/bridge/bridge.kt
@@ -1,45 +1,54 @@
-// Example for pattern "Bridge"
-// Alekseev Aleksei, group 271
+package patterns.Bridge
 
-package patterns.bridge
-
-import java.util.*
-
-/* Abstraction */
-public abstract class Image(internal var imageImp: ImageImp) {
-    init {
-        this.imageImp = imageImp
-    }
-    public fun getFormat() {
-        imageImp.format()
-    }
+interface Drawer {
+    fun drawCircle(x:Int, y:Int, radius:Int)
 }
 
-/* Implementor */
-public class Photo(imageImp: ImageImp) : Image(imageImp)
-
-public interface ImageImp {
-    public fun format()
+class SmallCircleDrawer:Drawer {
+    public override fun drawCircle(x:Int, y:Int, radius:Int) {
+        println("Small circle center = " + x + "," + y + " radius = " + radius * radiusMultiplier)
+    }
+    companion object {
+        val radiusMultiplier = 0.25
+    }
 }
+class LargeCircleDrawer:Drawer {
+    val radiusMultiplier = 10
 
-/* ConcreteImplementor */
-internal class Png : ImageImp {
-    public override fun format() {
-        println("Image format: Png")
+    public override fun drawCircle(x:Int, y:Int, radius:Int) {
+        println("Large circle center = " + x + "," + y + " radius = " + radius * radiusMultiplier)
     }
 }
 
-/* ConcreteImplementor */
-internal class Jpg : ImageImp {
-    public override fun format() {
-        println("Image format: Jpg")
+abstract class Shape protected constructor(drawer:Drawer) {
+    protected var drawer:Drawer
+    init{
+        this.drawer = drawer
+    }
+
+    abstract fun draw()
+    abstract fun enlargeRadius(multiplier:Int)
+}
+
+class Circle(var x:Int,var y:Int,var radius:Int, drawer:Drawer):Shape(drawer) {
+    public override fun draw() {
+        drawer.drawCircle(x, y, radius)
+    }
+    public override fun enlargeRadius(multiplier:Int) {
+        radius *= multiplier
     }
 }
 
-/* Client */
-fun main(args : Array<String>) {
-    val album = ArrayList<Photo>()
-    album.add(Photo(Png()))
-    album.add(Photo(Jpg()))
-    for (photo in album) {photo.getFormat()}
+fun main(argv:Array<String>){
+    val shapes = arrayOf<Shape>(
+            Circle(5, 10, 10, LargeCircleDrawer()),
+            Circle(20, 30, 100, SmallCircleDrawer())
+    )
+    for (shape in shapes)
+    {
+        shape.draw()
+    }
+    // Output :
+    // Large circle center = 5,10 radius = 100
+    // Small circle center = 20,30 radius = 25.0
 }
